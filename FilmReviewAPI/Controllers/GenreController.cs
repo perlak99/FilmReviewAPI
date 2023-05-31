83b4c5b9f0f27ecab30c8a4c7e739580c52ff2f5
Sławomir Perlak <perlakslawomir@gmail.com>
2023-05-31 18:45:22 +0200

    director and genre controller

---
+++ FilmReviewAPI/Controllers/GenreController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmReviewAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+    }
+}
